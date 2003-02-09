e012665f129082584a121ba295fc7d708f40d22b
autarch <devnull@localhost>
2003-02-09 18:46:48 +0000

    steal more languages from Graham ;)

---
+++ lib/DateTime/Language/Brazilian.pm
@@ -0,0 +1,22 @@
+##
+## Brazilian tables, contributed by Christian Tosta (tosta@cce.ufmg.br)
+##
+
+package Date::Language::Brazilian;
+
+use strict;
+
+use DateTime::Language;
+use vars qw(@ISA @DayNames @DayAbbreviations @MonthNames @MonthAbbreviations @AMPM);
+@ISA = qw(DateTime::Language);
+
+@DayNames = qw(Domingo Segunda Terça Quarta Quinta Sexta Sábado);
+@MonthNames = qw(Janeiro Fevereiro Março Abril Maio Junho
+	         Julho Agosto Setembro Outubro Novembro Dezembro);
+@DayAbbreviations = map { substr($_,0,3) } @DayNames;
+@MonthAbbreviations = map { substr($_,0,3) } @MonthNames;
+
+require DateTime::Language::English;
+@AMPM = @DateTime::Language::English::AMPM;
+
+1;
---
+++ lib/DateTime/Language/Danish.pm
@@ -0,0 +1,23 @@
+##
+## Danish tables
+##
+
+package Date::Language::Danish;
+
+use strict;
+
+use DateTime::Language;
+use vars qw(@ISA @DayNames @DayAbbreviations @MonthNames @MonthAbbreviations @AMPM);
+@ISA = qw(DateTime::Language);
+
+@MonthNames = qw(Januar Februar Marts April Maj Juni
+                 Juli August September Oktober November December);
+@DayNames = qw(Søndag Mandag Tirsdag Onsdag Torsdag Fredag Lørdag Søndag);
+@DayAbbreviations = map { substr($_,0,3) } @DayNames;
+@MonthAbbreviations = map { substr($_,0,3) } @MonthNames;
+
+require DateTime::Language::English;
+@AMPM = @DateTime::Language::English::AMPM;
+
+1;
+
