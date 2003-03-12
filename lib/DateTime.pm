4b22213de64aca6253bcc81027e691dd90332f96
autarch <devnull@localhost>
2003-03-12 23:36:12 +0000

    one more cosmetic tweak

--- lib/DateTime.pm
+++ lib/DateTime.pm
@@ -110,7 +110,8 @@ sub _calc_utc_rd
 
     delete $self->{utc_c};
 
-    if ( $self->{tz}->is_utc ) {
+    if ( $self->{tz}->is_utc )
+    {
         $self->{utc_rd_days} = $self->{local_rd_days};
         $self->{utc_rd_secs} = $self->{local_rd_secs};
 
