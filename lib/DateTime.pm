e0d8c17fe7b27334ad7420da486e1bbb2b2f97d0
autarch <devnull@localhost>
2003-05-30 17:34:42 +0000

    don't use plain offsets in time zone examples

--- lib/DateTime.pm
+++ lib/DateTime.pm
@@ -1745,11 +1745,11 @@ For example:
 
   my $dt = DateTime->new( year => 2000, month => 5, day => 10,
                           hour => 15, minute => 15,
-                          time_zone => '-0600', );
+                          time_zone => 'America/Los_Angeles', );
 
   print $dt->hour; # prints 15
 
-  $dt->set_time_zone( '-0400' );
+  $dt->set_time_zone( 'America/Chicago' );
 
   print $dt->hour; # prints 17
 
