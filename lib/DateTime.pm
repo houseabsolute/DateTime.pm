14a8443d8bca16590ef776e0e7f82dd4bc62d991
Olaf Alders <olaf@wundersolutions.com>
2023-01-11 11:35:37 -0500

    Update sample code for reducing local time zone lookups

--- lib/DateTime.pm
+++ lib/DateTime.pm
@@ -2522,11 +2522,11 @@ or elsewhere. If you know that the local time zone won't change while your code
 is running, and you need to make many objects for the local time zone, it is
 strongly recommended that you retrieve the local time zone once and cache it:
 
-    our $App::LocalTZ = DateTime::TimeZone->new( name => 'local' );
+    my $local_time_zone = DateTime::TimeZone->new( name => 'local' );
 
     # then everywhere else
 
-    my $dt = DateTime->new( ..., time_zone => $App::LocalTZ );
+    my $dt = DateTime->new( ..., time_zone => $local_time_zone );
 
 DateTime itself does not do this internally because local time zones can
 change, and there's no good way to determine if it's changed without doing all
